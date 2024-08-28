fab18b16abc4bffef40a3faa4112b164d1ce40c2
Yours Sincerely <tope@pressone.co>
2024-08-28 13:01:26 +0100

    assign number update

--- src/Telephony.php
+++ src/Telephony.php
@@ -7,27 +7,49 @@ use GuzzleHttp\Psr7\Request;
 class Telephony {
 
     private $baseUrl = "https://api.pressone.co/";
+    private $testBaseUrl = "https://pressone-internal-live.herokuapp.com/";
 
     private $accessToken = null;
+    private $isTest = false;
 
-    function __construct($key) {
+    function __construct($key, $isTest = false) {
         if (!$key) {
             throw new \Exception("Secret key needed for this object", 1);
         }
         $this->accessToken = $key;
+        $this->isTest = $isTest;
+    }
+
+    function availableNumbers($page = 1, $count = 100) {
+        $available_number = $this->get("api/available_number/numbers/?page_index=$page&page_size=$count");
+        $response = [
+            "data"  => [],
+            "total" => $available_number["total"],
+            "size"  => $available_number["page_size"]
+        ];
+
+        foreach ($available_number["data"] as $available_number_data) {
+            $number = [
+                "phone_number"  => $available_number_data["phone_number"],
+                "country_code"  => $available_number_data["country_code"]
+            ];
+
+            $response["data"][] = $number;
+        }
+
+        return $response;
     }
 
     function getNumbers($page = 1, $count = 100) {
-        $data = $this->get("api/third-party/sdk/number/");
+        $data = $this->get("api/third-party/sdk/number/?page_index=$page&page_size=$count");
         
         $response = [];
 
         foreach ($data as $number) {
-            // print_r($businessData);
             $response[] = [
                 "phone_number"  => $number["phone_number"],
                 "status"        => $number["verification_status"],
-                "label"         => $number["label"],
+                "label"         => $number["label"] ?? $number["phone_number"],
                 "number_id"     => $number["id"],
             ];
         }
@@ -36,7 +58,7 @@ class Telephony {
     }
 
     function getMembers($page = 1, $count = 100) {
-        $data = $this->get("api/third-party/sdk/team-member/");
+        $data = $this->get("api/third-party/sdk/team-member/?page_index=$page&page_size=$count");
         // 
         $responseData = $data["data"] ?? [];
         
@@ -83,7 +105,7 @@ class Telephony {
             "last_name"     => $data["last_name"] ?? $data["phone_number"],
             "email"         => $data["email"],
             "mobile"        => $data["phone_number"],
-            "role"          => $data["role"] ?? "owner",
+            "role"          => $data["role"] ?? "agent",
             "note"          => null,
             "country"       => $data["country"] ?? "NG",
             "can_make_calls"=> true,
@@ -101,21 +123,37 @@ class Telephony {
                 "can_view_performance_report"   => null,
                 "can_view_activity_report"      => null,
                 "business_numbers"  => $data["number_ids"],
-                "role"              => $data["role"] ?? "owner",
+                "role"              => $data["role"] ?? "agent",
             ]
         ];
 
         $data = $this->post("api/third-party/sdk/team-member/", $data);
 
+        if (isset($data["data"]) && $data["data"] === "400") {
+            return $data;
+        }
+
         $receivers = $data["receivers"] ?? [];
+        $profile = $data["profile"] ?? [];
 
         $response = [];
         foreach ($receivers as $receiver) {
             $response[] = [
-                "phone_number"  => $data["mobile"],
-                "full_name"     => $data["first_name"] . " " . $data["last_name"],
-                "receiver_id"   => $receiver["business_number"] ?? $receiver["id"],
+                "phone_number"  => $profile["mobile"],
+                "full_name"     => $profile["first_name"] . " " . $profile["last_name"],
+                "receiver_id"   => isset($receiver["business_number"]) ? $receiver["business_number"]["id"] : $receiver["id"],
                 "receiver_code" => $receiver["extension_code"],
+                "user"          => $profile["user"]
+            ];
+        }
+
+        if (count($response) === 0) {
+            $response[] = [
+                "phone_number"  => $profile["mobile"],
+                "full_name"     => $profile["first_name"] . " " . $profile["last_name"],
+                "receiver_id"   => null,
+                "receiver_code" => null,
+                "user_id"       => $profile["user"]
             ];
         }
 
@@ -126,6 +164,19 @@ class Telephony {
         return $this->post("api/third-party/sdk/receiver-line/", [
             "public_key"    => $public_key,
             "receiver"      => $receiver_id
+        ]);
+    }
+
+    function getCredentials($user_id) {
+        if (!$user_id) {
+            return [
+                "message"   => "The user ID is required for this request.",
+                "code"      => "400"
+            ];
+        }
+
+        return $this->post("api/third-party/sdk/team-member/login/", [
+            $user_id
         ]);
     }
 
@@ -147,6 +198,10 @@ class Telephony {
         return $this->makeRequest("POST", $url, $data);
     }
 
+    function getBaseUrl() {
+        return $this->isTest ? $this->testBaseUrl : $this->baseUrl;
+    }
+
     function makeRequest($method, $url, $body = null) {
         $headers = [
             'Authorization' => 'Bearer ' . $this->accessToken,
@@ -154,7 +209,7 @@ class Telephony {
         ];
 
         $client = new Client([
-            'base_uri'  => $this->baseUrl,
+            'base_uri'  => $this->getBaseUrl(),
             'headers'   => $headers
         ]);
 
@@ -178,7 +233,7 @@ class Telephony {
             }
 
             if ($statusCode > 300) {
-                throw new Exception("Error Processing Request", 1);
+                throw new \Exception("Error Processing Request", 1);
             }
 
             return $body;
@@ -186,7 +241,7 @@ class Telephony {
             $message = $e->getMessage();
             return [
                 "message"   => $message,
-                "code"      => "404"
+                "code"      => "400"
             ];
         }
     }
--- tests/PressoneTest.php
+++ tests/PressoneTest.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Pressone\Api\Telephony;
 
-$secret_key = "";
+$secret_key = "pk_43pAHHF2Wp45cQgfp4zD2rl3eHyoT_c4381679ea57c2e8ca0054b0dcd903c93aaa070486eed52b86b33205fe1b218b299d7605539237f28b645b7d5ea896b25bdef0728e60d6bf81b55a6373240df3";
 
 $pressone = new Telephony($secret_key);
 $numbers = $pressone->getNumbers();
